27464fdfdd9dafe7603b68e1c54efee42b74da66
Ariel Cid <ariel.cid@pedidosya.com>
2025-01-22 15:26:47 -0300

    wip

--- Core/Source/Script/Battle/Phase/Execution/ExecutionBuildingBlock.gd
+++ Core/Source/Script/Battle/Phase/Execution/ExecutionBuildingBlock.gd
@@ -1,6 +1,7 @@
 class_name ExecutionBuildingBlock
 
 enum FinishCondition {
+	INSTANT,
 	DURATION,
 	ANIMATION_FINISH
 }
@@ -10,3 +11,24 @@ var delay: float
 var duration: float
 var wait_after_finish: float
 var finish_condition: FinishCondition
+
+func set_values(dictionary: Dictionary):
+	delay = dictionary.get("delay")
+	duration = dictionary.get("duration")
+	wait_after_finish = dictionary.get("wait_after_finish")
+	
+	var finish_condition_key = dictionary.get("finish_condition")
+	finish_condition = _get_finish_condition(finish_condition_key)
+
+func _get_finish_condition(key: String) -> FinishCondition:
+	match key:
+		"instant":
+			return FinishCondition.INSTANT
+		"duration":
+			return FinishCondition.DURATION
+		"animation_finish":
+			return FinishCondition.ANIMATION_FINISH
+		_:
+			Assert.is_null_that_warns(key, "WARNING: Invalid key for block finish condition")
+	
+	return FinishCondition.INSTANT
